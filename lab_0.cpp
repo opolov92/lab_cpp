e115fe9513708e5a6ad76b70be1281fe4f98f58c
opolov92 <oleksijpolovincenko@gmail.com>
2024-09-15 18:56:08 +0200

    Create lab_0.cpp

---
+++ lab_0.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // Including cpp default library
+using namespace std; // Namespace definition
+
+int main() // Main function to run the program
+{
+    int a; // Variable definition
+    cout << "Enter value a : "; // Input value for calculations
+    cin >> a; // Console output message
+    int res = a - 5; // Calculations
+    cout << "a-5: "<< res; // Console output message
+    
+    return 0; // returning 0 as successfull programm completion
+}
